011656bfe5c6376e2fcf09ea43a9088844ebbba6
Tom Claudio <dev.tomclaudio@gmail.com>
2021-03-20 15:08:23 +0800

    Create project boilerplace

---
+++ README.md
@@ -0,0 +1,5 @@
+# JSX Parse
+
+This is a personal exploratory project to teach myself how a JSX parser works.
+
+I might even use this as my JSX parser for another project where I tried to create my own React library.
---
+++ index.js
@@ -0,0 +1,13 @@
+function parse(str) {
+  console.log(str)
+  // get the opening tag
+  // get the closing tag
+  return {
+    type: 'type',
+    children: []
+  }
+}
+
+parse('<h1>Hello, world</>')
+
+module.exports = parse
