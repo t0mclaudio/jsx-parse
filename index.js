61a7e6628589625b0bfe4f98f1f9b6dc4f92949e
Tom Claudio <dev.tomclaudio@gmail.com>
2021-03-21 19:06:34 +0800

    feat: work on parser

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 function parse(str) {
   console.log(str)
+  let arr = [...str] // make string an array
+  let arrLength = arr.length
+  let head;
+  let tail;
+
+  do {
+    head = arr[0]
+    tail = arr[arrLength - 1]
+    arr = [...arr.slice(1, arr.length -1)]
+    arrLength = arr.length
+    if (head === '<' && tail === '>') {
+      console.log('opened new tag')
+      let thisTag = ''
+      let i = 0
+      while (arr[i] !== '>') {
+        thisTag += arr[i]
+        i++ 
+      } 
+    } else {
+      console.log('inside tag')
+    }
+  } while (arrLength)
+  
+  
   // get the opening tag
   // get the closing tag
   return {
@@ -8,6 +32,6 @@ function parse(str) {
   }
 }
 
-parse('<h1>Hello, world</>')
+parse('<h1>Hello, world<h1/>')
 
 module.exports = parse
