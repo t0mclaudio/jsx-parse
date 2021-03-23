a68dcbb098e351c8c245e4e4e3ac88d22be7fe7e
Tom Claudio <dev.tomclaudio@gmail.com>
2021-03-23 19:40:40 +0800

    try

--- index.js
+++ index.js
@@ -5,27 +5,40 @@ function parse(str) {
   let head;
   let tail;
 
-  do {
-    head = arr[0]
-    tail = arr[arrLength - 1]
-    if (head === '<' && tail === '>') {
-      console.log('Opened new tag')
-      let thisTag = ''
-      let i = 1
-      while (arr[i] !== '>') {
-        thisTag += arr[i]
-        i++ 
-      }
-      arr = [...arr.slice(thisTag.length + 1, arr.length - (thisTag.length + 2))] // to accommodate for 2 chars />
-      console.log("The current tag >>", thisTag)
-      // TODO: handle tag attributes
-    } else {
-      arr = [...arr.slice(1, arr.length -1)]
-      arrLength = arr.length
-      console.log('Inside a tag', arr)
-    }
-  } while (arrLength)
-  
+  // do {
+  //   head = arr[0]
+  //   tail = arr[arrLength - 1]
+  //   if (head === '<' && tail === '>') {
+  //     console.log('Opened new tag')
+  //     let thisTag = ''
+  //     let i = 1
+  //     while (arr[i] !== '>') {
+  //       thisTag += arr[i]
+  //       i++ 
+  //     }
+  //     arr = [...arr.slice(thisTag.length + 1, arr.length - (thisTag.length + 2))] // to accommodate for 2 chars />
+  //     console.log("The current tag >>", thisTag)
+  //     // TODO: handle tag attributes
+      
+  //   } else {
+  //     arr = [...arr.slice(1, arr.length -1)]
+  //     arrLength = arr.length
+  //     // console.log('Inside a tag', arr)
+  //     for (let i=0; i < arrLength; i++) {
+  //       console.log(arr[i])
+  //     }
+  //   }
+  // } while (arrLength)
+  for (let i=0; i < str.length; i++) {
+    currentChar = str.charAt(i)
+    nextChar = str.charAt(i + 1)
+    if (currentChar === ' ') continue
+    if (currentChar === '<') console.log('open tag', currentChar)
+    if (currentChar === '{') console.log('open brace', currentChar)
+    if (currentChar === '/' && nextChar === '>') console.log('closing tag', currentChar+nextChar)
+    if (currentChar === '}') console.log('closing brace', currentChar)
+    
+  }
   
   // get the opening tag
   // get the closing tag
